14c501c4d7c2647bcac8a30841d481c9cff236b7
mr-rider <mr.rider2@mail.ru>
2022-12-15 12:17:16 +0800

    added requests to flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,23 +1,21 @@
 # flaskapp.py
 
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    r = requests.get('https://api.thingspeak.com/channels/254616/fields/1/last.txt')
+    temp_c_in = r.text
+    temp_f = str(round(((9.0 / 5.0) * float(temp_c_in) + 32), 1)) + ' F'
+    return render_template("index.html", temp=temp_f)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
 
 
-@app.route("/")
-def index():
-    return "<h1>The temperature is 30.5 C</h1>"
 
-
-if __name__=="__main__":
-    app.run(host='0.0.0.0')
