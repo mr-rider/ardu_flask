83c919f549539870427edacb474591ceb07aad33
mr-rider <mr.rider2@mail.ru>
2022-12-16 19:48:39 +0800

    change flaskapp for updating temperature

--- flaskapp.py
+++ flaskapp.py
@@ -10,8 +10,14 @@ app = Flask(__name__)
 def index():
     r = requests.get('https://api.thingspeak.com/channels/254616/fields/1/last.txt')
     temp_c_in = r.text
-    temp_f = str(round(((9.0 / 5.0) * float(temp_c_in) + 32), 1)) + ' F'
-    return render_template("index.html", temp=temp_f)
+    temp_c_in = str(round(float(temp_c_in),1)) + ' C'
+    #temp_f = str(round(((9.0 / 5.0) * float(temp_c_in) + 32), 1)) + ' F'
+    return render_template("index.html", temp=temp_c_in)
+
+
+@app.route("/update/API_key=<api_key>/mac=<mac>/field=<int:field>/data=<data>", methods=['GET'])
+def update(api_key, mac, field, data):
+    return render_template("update.html", data=data)
 
 
 if __name__ == "__main__":
